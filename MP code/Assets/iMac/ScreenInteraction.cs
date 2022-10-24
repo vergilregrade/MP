2c9dafe52a473151fda8768d426a60d88b8d9848
Renald <weisbrice@gmail.com>
2022-10-24 22:39:16 +0200

    remake computer

--- MP code/Assets/iMac/ScreenInteraction.cs
+++ MP code/Assets/iMac/ScreenInteraction.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class ScreenInteraction : MonoBehaviour
 {
-    private bool enable;
+    private bool enable = false;
 
-    private int nbColonne;
-    private int nbLingne;
-    private int currentLigne;
-    private int currentColonne;
-    private string screen;
+    private int nbColonne = 26;
+    private int nbLingne = 7;
+    private int currentLigne = 0;
+    private int currentColonne = 0;
+    //private string screen;
     private char[,] monitor;
     public TextMeshPro textDisplay;
 
@@ -21,50 +22,27 @@ public class ScreenInteraction : MonoBehaviour
     private bool playerCollision;
     private GameObject player;
     public GameObject o_monitor;
-    // Start is called before the first frame update
+
+
+    string head = ">";
+    string currentCommand = "";
+
+
+
+
+
     void Start()
     {
-        inSwitchMode = false;
-        enable = false;
-        //currentLigne = 0;
-        //currentColonne = 0;
-        nbLingne = 7;
-        nbColonne = 26;//-7;
-        screen = "ok";
-        textDisplay.text = "Example Text";
         monitor = new char[nbLingne, nbColonne + 1];
-        //for (int i = 0; i < nbLingne; i++)
-        //{
-        //    for (int j = 0; j < nbColonne + 1; j++)
-        //    {
-        //        if (j == nbColonne)
-        //            monitor[i, j] = (char)0;
-        //        else
-        //            monitor[i, j] = ' ';
-        //    }
-        //}
         clearScreen();
-        string to_print = "";
-        for (int i = 0; i < nbLingne; i++)
-        {
-            if (i == currentLigne)
-                to_print += (inSwitchMode)? "switch>":">";
-            for (int j = 0; j < nbColonne; j++)
-            {
-
-                to_print += monitor[i, j];
-            }
-            to_print += '\n';
-        }
-        textDisplay.text = to_print;
+        printMonitor();
+        
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (enable)
         {
-            if(Input.GetKeyDown(KeyCode.Escape))
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
                 enable = false;
                 player.GetComponent<Move_Personnage>().setEnable(true);
@@ -72,93 +50,212 @@ public class ScreenInteraction : MonoBehaviour
                 o_monitor.GetComponent<Mac_collision_trigger>().setCamEnable(false);
                 return;
             }
-
-            string to_print = "";
-            for (int i = 0; i < nbLingne; i++)
+            string screen = Input.inputString;
+            switch(screen[0])
             {
-                if (i == currentLigne)
-                    to_print += (inSwitchMode) ? "switch>" : ">";
-                for (int j = 0; j < nbColonne; j++)
-                {
-
-                    to_print += monitor[i, j];
-                }
-                to_print += '\n';
+                case (char) 0:
+                    return;
+                case (char) 13://enter
+                    nextCurrentLine(currentCommand);
+                    action();
+                    currentCommand = "";
+                    printToMonitor(head);
+                    break;
+                case (char) 8://bask space
+                    currentCommand = currentCommand.Remove(currentCommand.Length - 1);
+                    break;
+                default:
+                    currentCommand += screen[0];
+                    break;
             }
-            textDisplay.text = to_print;
+            clearCurrentLine(); printToMonitor(head);
+            printToMonitor(currentCommand);
+            printMonitor();
 
-            screen = Input.inputString;
-            foreach (char c in screen)
-            {
-                //print((int)c );
-                if ((int)c == 13)//enter
-                {
-                    monitor[currentLigne, nbColonne] = (char)currentColonne;
-                    string ret = traitementLigne(currentLigne);
-                    if (ret == "clear")
-                    {
-                        clearScreen();
-                        return;
-                    }
-                    
-                    if (currentLigne+2 == nbLingne || currentLigne + 1 == nbLingne)
-                    {
-                        for (int i = 0; i < nbLingne; i++)
-                        {
-                            for (int j = 0; j < nbColonne + 1; j++)
-                            {
-                                if(i==nbLingne-1 || i == nbLingne - 2)
-                                {
-                                    monitor[i, j] = (j == nbColonne) ? (char)0 : ' ';
-                                }
-                                else
-                                    monitor[i, j] = monitor[i + 2, j];
-                            }
-                        }
-                    }
-                    else
-                        currentLigne = (currentLigne + 2) % nbLingne;
-                    currentColonne = 0;//(int)monitor[currentLigne, nbColonne];
-                    for(int i=0;i<nbColonne+1;i++)
-                    {
-                        monitor[currentLigne - 1, i] = (i<ret.Length)?ret[i]:' ';
-                    }
-                }
-                else if ((int)c == 8)//supr
-                {
-                    if(currentColonne == nbColonne-1 && monitor[currentLigne, currentColonne] != ' ')
-                        monitor[currentLigne, currentColonne] = ' ';
-                    else
-                    {
-                        currentColonne = (currentColonne - 1 >= 0) ? currentColonne - 1 : 0;// (currentColonne - 1) % nbColonne;
-                        monitor[currentLigne, currentColonne] = ' ';
-                    }
-                    
-                }
-                else
-                {
-                    monitor[currentLigne, currentColonne] = c;
-                    if (currentColonne != nbColonne - 1)
-                        currentColonne++; //= (currentColonne + 1) % nbColonne;
-                }
-                //print(c + " "+currentLigne.ToString()+ " " + currentColonne.ToString());
-            }
-
-            if (screen != "")
-            {
-                textDisplay.text = screen;
-
-            }
         }
-        //else
-        //{
-        //    if(playerCollision && Input.GetKeyDown(KeyCode.E))
-        //    {
-        //        enable = true;
-        //        player.GetComponent<Move_Personnage>().setEnable(false);
-        //    }
-        //}
     }
+    private void printToMonitor(String s)
+    {
+        string toPrint = "";
+        int total_len = currentColonne + s.Length;
+        toPrint += (total_len > nbColonne) ? s.Substring(total_len - nbColonne) : s;
+        foreach (char c in toPrint)
+        {
+            monitor[currentLigne, currentColonne] = c;
+            currentColonne++;
+        }
+        
+    }
+    private void printMonitor()
+    {
+        string to_print = "";
+        for (int y = 0; y < nbLingne; y++)
+        {
+            for (int x = 0; x < nbColonne; x++)
+            {
+                to_print += monitor[y, x];
+            }
+            to_print += '\n';
+        }
+        textDisplay.text = to_print;
+    }
+    private void nextCurrentLine(string s)
+    {
+        clearCurrentLine();
+        printToMonitor(s);
+        currentLigne++;
+        currentColonne = 0;
+    }
+    private void clearCurrentLine()
+    {
+        for(int x = 0; x < nbColonne; x++)
+        {
+            monitor[currentLigne, x] = ' ';
+        }
+        currentColonne = 0;        
+    }
+    private void action()
+    {
+        nextCurrentLine("Oki");
+
+    }
+    //void Start()
+    //{
+    //    inSwitchMode = false;
+    //    enable = false;
+    //    //currentLigne = 0;
+    //    //currentColonne = 0;
+    //    nbLingne = 7;
+    //    nbColonne = 26;//-7;
+    //    screen = "ok";
+    //    textDisplay.text = "Example Text";
+    //    monitor = new char[nbLingne, nbColonne + 1];
+    //    //for (int i = 0; i < nbLingne; i++)
+    //    //{
+    //    //    for (int j = 0; j < nbColonne + 1; j++)
+    //    //    {
+    //    //        if (j == nbColonne)
+    //    //            monitor[i, j] = (char)0;
+    //    //        else
+    //    //            monitor[i, j] = ' ';
+    //    //    }
+    //    //}
+    //    clearScreen();
+    //    string to_print = "";
+    //    for (int i = 0; i < nbLingne; i++)
+    //    {
+    //        if (i == currentLigne)
+    //            to_print += (inSwitchMode)? "switch>":">";
+    //        for (int j = 0; j < nbColonne; j++)
+    //        {
+
+    //            to_print += monitor[i, j];
+    //        }
+    //        to_print += '\n';
+    //    }
+    //    textDisplay.text = to_print;
+    //}
+
+
+    //void Update()
+    //{
+    //    if (enable)
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.Escape))
+    //        {
+    //            enable = false;
+    //            player.GetComponent<Move_Personnage>().setEnable(true);
+    //            player.GetComponent<Move_Personnage>().SetCamEnable(true);
+    //            o_monitor.GetComponent<Mac_collision_trigger>().setCamEnable(false);
+    //            return;
+    //        }
+
+    //        string to_print = "";
+    //        for (int i = 0; i < nbLingne; i++)
+    //        {
+    //            if (i == currentLigne)
+    //                to_print += (inSwitchMode) ? "switch>" : ">";
+    //            for (int j = 0; j < nbColonne; j++)
+    //            {
+
+    //                to_print += monitor[i, j];
+    //            }
+    //            to_print += '\n';
+    //        }
+    //        textDisplay.text = to_print;
+
+    //        screen = Input.inputString;
+    //        foreach (char c in screen)
+    //        {
+    //            //print((int)c );
+    //            if ((int)c == 13)//enter
+    //            {
+    //                monitor[currentLigne, nbColonne] = (char)currentColonne;
+    //                string ret = traitementLigne(currentLigne);
+    //                if (ret == "clear")
+    //                {
+    //                    clearScreen();
+    //                    return;
+    //                }
+
+    //                if (currentLigne + 2 == nbLingne || currentLigne + 1 == nbLingne)
+    //                {
+    //                    for (int i = 0; i < nbLingne; i++)
+    //                    {
+    //                        for (int j = 0; j < nbColonne + 1; j++)
+    //                        {
+    //                            if (i == nbLingne - 1 || i == nbLingne - 2)
+    //                            {
+    //                                monitor[i, j] = (j == nbColonne) ? (char)0 : ' ';
+    //                            }
+    //                            else
+    //                                monitor[i, j] = monitor[i + 2, j];
+    //                        }
+    //                    }
+    //                }
+    //                else
+    //                    currentLigne = (currentLigne + 2) % nbLingne;
+    //                currentColonne = 0;//(int)monitor[currentLigne, nbColonne];
+    //                for (int i = 0; i < nbColonne + 1; i++)
+    //                {
+    //                    monitor[currentLigne - 1, i] = (i < ret.Length) ? ret[i] : ' ';
+    //                }
+    //            }
+    //            else if ((int)c == 8)//supr
+    //            {
+    //                if (currentColonne == nbColonne - 1 && monitor[currentLigne, currentColonne] != ' ')
+    //                    monitor[currentLigne, currentColonne] = ' ';
+    //                else
+    //                {
+    //                    currentColonne = (currentColonne - 1 >= 0) ? currentColonne - 1 : 0;// (currentColonne - 1) % nbColonne;
+    //                    monitor[currentLigne, currentColonne] = ' ';
+    //                }
+
+    //            }
+    //            else
+    //            {
+    //                monitor[currentLigne, currentColonne] = c;
+    //                if (currentColonne != nbColonne - 1)
+    //                    currentColonne++; //= (currentColonne + 1) % nbColonne;
+    //            }
+    //            //print(c + " "+currentLigne.ToString()+ " " + currentColonne.ToString());
+    //        }
+
+    //        if (screen != "")
+    //        {
+    //            textDisplay.text = screen;
+
+    //        }
+    //    }
+    //    //else
+    //    //{
+    //    //    if(playerCollision && Input.GetKeyDown(KeyCode.E))
+    //    //    {
+    //    //        enable = true;
+    //    //        player.GetComponent<Move_Personnage>().setEnable(false);
+    //    //    }
+    //    //}
+    //}
 
 
     private string traitementLigne(int cL)
@@ -166,11 +263,26 @@ public class ScreenInteraction : MonoBehaviour
         string ligne = "";
         for(int i=0;i<nbColonne;i++)
         {
-            if (monitor[cL, i] != ' ')
-            {
+            //if (monitor[cL, i] != ' ')
+            //{
                 ligne += monitor[cL, i];
+            //}
+        }
+        string[] strList = ligne.Split(' ');
+
+        if(strList.Length > 0)
+        {
+
+            switch(strList[0])
+            {
+                case "bitE":
+                    return "UwU";
+                case "enable":
+                    return (enableCisco(strList, 1));
             }
         }
+        
+
 
         if(ligne == "bitE")
         {
@@ -203,16 +315,24 @@ public class ScreenInteraction : MonoBehaviour
     {
         currentLigne = 0;
         currentColonne = 0;
-        for (int i = 0; i < nbLingne; i++)
+        //for (int i = 0; i < nbLingne; i++)
+        //{
+        //    for (int j = 0; j < nbColonne + 1; j++)
+        //    {
+        //        if (j == nbColonne)
+        //            monitor[i, j] = (char)0;
+        //        else
+        //            monitor[i, j] = ' ';
+        //    }
+        //}
+        for(int y = 0; y < nbLingne;y++)
         {
-            for (int j = 0; j < nbColonne + 1; j++)
+            for(int x = 0; x < nbLingne; x++)
             {
-                if (j == nbColonne)
-                    monitor[i, j] = (char)0;
-                else
-                    monitor[i, j] = ' ';
+                monitor[y, x] = ' ';
             }
         }
+        printToMonitor(head);
     }
     //public void setPlayerTriggerEnter(GameObject p)
     //{
@@ -229,6 +349,16 @@ public class ScreenInteraction : MonoBehaviour
     {
         player = p;
         enable = true;
+    }
+
+    private string enableCisco(string[] str, int i)
+    {
+        if (inSwitchMode)
+        {
+            return "error";
+        }
+        inSwitchMode = true;
+        return "clear";
     }
 
 }
